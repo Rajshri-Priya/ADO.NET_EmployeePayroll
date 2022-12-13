4bb274cfd95c575e5127370b7387ed9d582eb4d6
rajshri-priya <rajshri.priya1708@.gmail.com>
2022-12-13 15:53:11 +0530

    Ado.net payroll service added

---
+++ ADO.net_Payroll_services/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ADO.net_Payroll_services
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom To SQL Program");
+            EmployeeRepository.GetAllEmployee();
+            Console.ReadLine();
+        }
+    }
+}
